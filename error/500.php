0b55b7d8ec2ae6490997559bf429330dd3934bdf
mproncace <mproncace@gmail.com>
2015-01-16 17:02:58 -0500

    Rewrite 500 error document

--- error/500.php
+++ error/500.php
@@ -1,10 +1,9 @@
 <?php
 require('../templates/header.php');
 ?>
-<p>A grave mistake has been made on our server. We cannot say whether we are responsible, as only the logs may hold that precious information. The
-server may be over capacity, or perhaps the program has encountered a rather nasty bug. Or, most likely, we've made a mistake in our code. You could
-try refreshing the page, but if that doesn't fix the problem, then please <a href="mailto:admin@caseif.net">feel free to send us an email</a>. Be sure
-to include the current time and the URL you were attempting to access, though, or we won't be able to help.</p>
+<p>A 500 response code means something on our server blew up. We're most likely already running around with our hair on fire, but just in case, please
+<a href="mailto:admin@caseif.net">feel free to send us an email</a>. Be sure to include the URL you were attempting to access, though, or we won't be
+able to help.</p>
 <div class="massive">500</div>
 <?php
 require('../templates/footer.php');

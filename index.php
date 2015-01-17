95c6663a0b05d8a49a8c4353e34b76aa05fee752
mproncace <mproncace@gmail.com>
2015-01-16 23:40:04 -0500

    Properly format info in index.php

--- index.php
+++ index.php
@@ -2,20 +2,18 @@
 require('./templates/header.php');
 ?>
 		Flares that Care is a quarterly (4 times/year) charity livestream supporting Child's Play Charity founded by
-		<a href="http://steamcommunity.com/profiles/76561198158123097" target="_blank">B_RED</a> and
+		<p><a href="http://steamcommunity.com/profiles/76561198158123097" target="_blank">B_RED</a> and
 		<a href="http://steamcommunity.com/profiles/76561198155316177" target="_blank">Snoven</a> in December of 2014. Our first event raised $500,
 		way above our original goal of $300. Our second event will be held in March of 2015. Our hope is to raise $750 and dedicate as much time into
-		it, rather than the rushed 3 days of planning we had before our first event. 
-		<br><br>
-		Our second event is set to take place from Friday, March 13th at 12 PM PST until Sunday, March 15th at 12 AM PST.
-		<a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to check out what events will be happening during the
-		stream and who will be attending.
-		<br><br>
-		Our stream will be held <a href="http://www.twitch.tv/flaresthatcare">at this link</a>. Please follow to show your support and to know when we
-		go live!
-		<br><br>
-		Our website isn't finished yet as this is all being done on a volunteer basis for charity, but we're doing our best.
-		For those that don't know what Child's Play is, <a href="http://www.childsplaycharity.org/">you can find their site here.</a>
+		it, rather than the rushed 3 days of planning we had before our first event.</p>
+		<p>Our second event is set to take place from Friday, March 13th at 12 PM PST until Sunday, March 15th at 12 AM PST.</p>
+		<p><a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to check out what events will be happening during
+		the stream and who will be attending.</p>
+		<p>Our stream will be held <a href="http://www.twitch.tv/flaresthatcare">at this link</a>. Please follow to show your support and to know when
+		we go live!</p>
+		<p>For those who don't know what Child's Play is, <a href="http://www.childsplaycharity.org/">you can find their site here.</a></p>
+		<p>Note: Our website is currently still under construction and may be a bit quirky at times; bear with us until we reach a finalized version.
+		</p>
 <?php
 require('./templates/footer.php');
 ?>

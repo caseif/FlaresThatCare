f63df87836c875f69993b71f6080e80ab6c889de
mproncace <mproncace@gmail.com>
2015-01-16 22:58:19 -0500

    Add gradient to navbar background

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
 				<hr>
 				<br>
 				&#169; Copyright 2015 Flares That Care
-				<span style="padding-right:20%;"></span> <!-- spacer -->
+				<span style="padding-right:7%;"></span> <!-- spacer -->
 				<a href="https://github.com/caseif/FlaresThatCare" target="_blank">Source Code</a>
 			</div>
 		<!-- /container div -->

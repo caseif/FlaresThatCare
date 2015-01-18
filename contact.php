71360c5ef5ee30693537d249e623fe0e2c3a90a0
caseif <mproncace@gmail.com>
2015-01-18 05:42:00 +0100

    Add files missing from previous commits

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php
+$pageTitle = 'Contact Us - Flares that Care';
+require('./templates/header.php');
+?>
+We'd love to hear your feedback! We can be reached at the following addresses:
+<p>
+	<b>General Inquiries</b>
+	<br>
+	<a href="mailto:flares@flaresthatcare.org">flares@flaresthatcare.org</a>
+	<br>
+	<a href="mailto:nick@flaresthatcare.org">nick@flaresthatcare.org</a>
+	<br>
+	<a href="mailto:snoven@flaresthatcare.org">snoven@flaresthatcare.org</a>
+</p>
+<p>
+	<b>Management Inquiries</b>
+	<br>
+	<a href="mailto:pops@flaresthatcare.org">pops@flaresthatcare.org</a>
+<p>
+	<b>Technical Inquiries</b>
+	<br>
+	<a href="mailto:maxim@flaresthatcare.org">maxim@flaresthatcare.org</a>
+</p>
+<p>
+	<b>Audio/Video Inquiries</b>
+	<br>
+	<a href="mailto:jerry@flaresthatcare.org">jerry@flaresthatcare.org</a>
+</p>
+<?php
+require('./templates/footer.php');
+?>

0c468fb51b6f665fd39b0285b1e8b8c37ac262d3
caseif <mproncace@gmail.com>
2015-01-27 23:12:33 +0100

    Update backgrounds of header and footer banners

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ require('./templates/header.php');
 		<p>Flares that Care is a quarterly (4 times/year) charity livestream supporting Child's Play Charity founded by
 		<a href="http://steamcommunity.com/profiles/76561198158123097" target="_blank">B_RED</a> and
 		<a href="http://steamcommunity.com/profiles/76561198155316177" target="_blank">Snoven</a> in December of 2014. Our first event raised $500,
-		way above our original goal of $300. Our second event will be held in March of 2015. Our hope is to raise $750 and dedicate as much time into
-		it, rather than the rushed 3 days of planning we had before our first event.</p>
+		far above our original goal of $300. Our second event will be held in March of 2015. Our hope is to raise $750 and dedicate as much time
+		to preparing for it as possible, rather than the rushed 3 days of planning we had before our first event.</p>
 		<p>Our second event is set to take place from Friday, March 13th at 12 PM PST until Sunday, March 15th at 12 AM PST.</p>
 		<p><a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to check out what events will be happening during
 		the stream and who will be attending.</p>

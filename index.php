10443ef33bc05f4e0806bc2967085dad8ba2c2e7
caseif <mproncace@gmail.com>
2015-02-16 03:05:41 +0100

    Remove Under Construction notice from homepage

--- index.php
+++ index.php
@@ -10,13 +10,11 @@ require('./templates/header.php');
 		far above our original goal of $300. Our second event will be held in March of 2015. Our hope is to raise $750 and dedicate as much time
 		to preparing for it as possible, rather than the rushed 3 days of planning we had before our first event.</p>
 		<p>Our second event is set to take place from Friday, March 13th at 12 PM PST until Sunday, March 15th at 12 AM PST.</p>
-		<p><a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to check out what events will be happening during
-		the stream and who will be attending.</p>
+		<p><a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to see what events will be happening during the
+		stream and who will be attending.</p>
 		<p>Our stream will be held <a href="http://www.twitch.tv/flaresthatcare">at this link</a>. Please follow to show your support and to know when
 		we go live!</p>
 		<p>For those who don't know what Child's Play is, <a href="http://www.childsplaycharity.org/">you can find their site here.</a></p>
-		<p>Note: Our website is currently still under construction and may be a bit quirky at times; bear with us until we reach a finalized version.
-		</p>
 	</div>
 </div>
 <div id="section-schedule">

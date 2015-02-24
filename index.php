f57f21eb0fb5ffe1045d84e016e666dae265e060
caseif <mproncace@gmail.com>
2015-02-24 02:51:11 +0100

    Add donate button

--- index.php
+++ index.php
@@ -6,21 +6,36 @@ require('./templates/header.php');
 	<div class="content">
 		<p>Flares that Care is a quarterly (4 times/year) charity livestream supporting Child's Play Charity founded by
 		<a href="http://steamcommunity.com/profiles/76561198158123097" target="_blank">B_RED</a> and
-		<a href="http://steamcommunity.com/profiles/76561198155316177" target="_blank">Snoven</a> in December of 2014. Our first event raised $500,
-		far above our original goal of $300. Our second event will be held in March of 2015. Our hope is to raise $750 and dedicate as much time
-		to preparing for it as possible, rather than the rushed 3 days of planning we had before our first event.</p>
-		<p>Our second event is set to take place from Friday, March 13th at 12 PM PST until Sunday, March 15th at 12 AM PST.</p>
-		<p><a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to see what events will be happening during the
-		stream and who will be attending.</p>
-		<p>Our stream will be held <a href="http://www.twitch.tv/flaresthatcare">at this link</a>. Please follow to show your support and to know when
-		we go live!</p>
-		<p>For those who don't know what Child's Play is, <a href="http://www.childsplaycharity.org/">you can find their site here.</a></p>
+		<a href="http://steamcommunity.com/profiles/76561198155316177" target="_blank">Snoven</a> in December of 2014.
+		Our first event raised $500, far above our original goal of $300. Our second event will be held in March of
+		2015. Our hope is to raise $750 and dedicate as much time to preparing for it as possible, rather than the
+		rushed 3 days of planning we had before our first event.</p>
+		<p>Our second event is set to take place from Friday, March 13th at 12 PM PST until Sunday, March 15th at 12 AM
+		PST.</p>
+		<p><a href="http://steamcommunity.com/groups/flaresthatcare">Check out our Steam group</a> to see what events
+		will be happening during the stream and who will be attending.</p>
+		<p>Our stream will be held <a href="http://www.twitch.tv/flaresthatcare">at this link</a>. Please follow to show
+		your support and to know when we go live!</p>
+		<p>For those who don't know what Child's Play is, <a href="http://www.childsplaycharity.org/">you can find their
+		site here.</a></p>
 	</div>
 </div>
 <div class="section right">
 	<div class="ribbon"><span>Event Schedule</span></div>
 	<div class="content">
-		<p>A link to our current event schedule may be found <a href="http://goo.gl/DdOmFz" target="_blank">here</a>.</p>
+		<p>A link to our current event schedule may be found <a href="http://goo.gl/DdOmFz" target="_blank">here</a>.
+		</p>
+	</div>
+</div>
+<div class="section left">
+	<div class="ribbon"><span>Donations</span></div>
+	<div class="content">
+		We are accepting general donations on
+		<a href="https://donate.childsplaycharity.org/0db6a8ab44425094824538ec7126aa7f">this page</a>, but we also need
+		funds to keep our website and servers running. You can donate to this cause using the yellow "Donate" button
+		below. We are a non-profit run by volunteers, so we promise we'll never use these funds for anything but to
+		subsidize the costs of running the event.
+		<p><?php include('./donate_button.html'); ?></p>
 	</div>
 </div>
 <?php
---
+++ thankyou.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = 'Thank You! - Flares that Care';
+require('./templates/header.php');
+?>
+Thank you for donating! It really helps us out a lot. We promise we won't use your money for anything other than event
+costs.
+<p><a href="/">Back to Home</a></p>
+<?php
+require('./templates/footer.php');
+?>
